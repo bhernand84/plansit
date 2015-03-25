fc7860c78bc037bef5b8a4f95c1201429cbdf88e
Benaldo Hernandez <bhernandez@tonicdesign.com>
2015-03-25 09:50:55 -0400

    Reorganize users

--- models/place.go
+++ models/place.go
@@ -1,9 +1,27 @@
 package models
 
+import(
+	"time"
+)
+
 type Place struct{
 	Place_id string
 	Notes string
 	UserID string
 }
 
+type Trip struct{
+	Name string
+	DateCreated time.Time
+	Description string
+	Departure time.Time
+	Length	string
+	Places []Place
+}
+type PlansitUser struct {
+	Userid string
+	Name string
+	Email string
+	Trips []Trip
+}
 
--- plansit.go
+++ plansit.go
@@ -8,12 +8,13 @@ import(
 	"appengine"
 	"appengine/user"
 	"appengine/datastore"
+	"time"
 )
 
 var (
 	indexTemplate = template.Must(template.ParseFiles("views/index.html"))
 	mapperTemplate = template.Must(template.ParseFiles("views/mapper.html"))
-	currentUser plansitUser
+	currentUser *models.PlansitUser
 )
 func init(){
 	http.HandleFunc("/",root)
@@ -25,19 +26,19 @@ func init(){
 }
 
 func root(w http.ResponseWriter, r *http.Request){
-	currentUser.checkAuth(w,r)
+	checkAuth(w,r)
 	myPlaces := GetPlace(r)
 
 	indexTemplate.Execute(w, myPlaces)
 }
 
 func mapper( w http.ResponseWriter, r *http.Request){
-	currentUser.checkAuth(w,r)
+	checkAuth(w,r)
 	mapperTemplate.Execute(w, "")
 }
 func addPlace(w http.ResponseWriter, r * http.Request){
  	placeid, notes := r.FormValue("placeid"), r.FormValue("notes")
- 	currentUser.checkAuth(w, r)
+ 	checkAuth(w, r)
  	place := &models.Place{placeid, notes, currentUser.Userid}	
  	PutPlace(r, place)
 
@@ -45,13 +46,7 @@ func addPlace(w http.ResponseWriter, r * http.Request){
 
 }
 
-type plansitUser struct {
-	Userid string
-	Name string
-	Email string
-}
-
-func (currentUser *plansitUser) checkAuth(w http.ResponseWriter, r *http.Request){ 
+func checkAuth(w http.ResponseWriter, r *http.Request){ 
 	c:= appengine.NewContext(r)
 	u:= user.Current(c)
 	if u == nil {
@@ -65,14 +60,18 @@ func (currentUser *plansitUser) checkAuth(w http.ResponseWriter, r *http.Request
         return 
     }
     GetUser(r, u)
-    if currentUser == nil{
-    	newUser := &plansitUser{u.ID, "hi", u.Email}
+    if currentUser != nil{
+    	var myTrip []models.Trip
+    	var myPlaces []models.Place
+    	myPlaces[0] = models.Place{"91981981","a nice place","111111"}
+    	myTrip[0] = models.Trip{"My trip",time.Now(), "my tupe is gonna be awesome", time.Now(), "15 days", myPlaces }
+    	newUser := &models.PlansitUser{u.ID, "hi", u.Email, nil}
     	PutUser(r, newUser)
-   	currentUser = &plansitUser{u.ID, "hi", u.Email}
-	}
+   	currentUser = &models.PlansitUser{u.ID, "hi", u.Email, nil}
+   }
 }
 
-func PutUser(r *http.Request, myUser *plansitUser){
+func PutUser(r *http.Request, myUser *models.PlansitUser){
 	c:= appengine.NewContext(r)
 	key := datastore.NewIncompleteKey(c, "User", userKey(c))
 	_, err := datastore.Put(c, key, myUser)
@@ -84,13 +83,13 @@ func PutUser(r *http.Request, myUser *plansitUser){
 func GetUser(r *http.Request, u *user.User) {
 	c:= appengine.NewContext(r)
 	q := datastore.NewQuery("User").Ancestor(userKey(c)).Filter("Userid=", u.ID)
-	var newUsers []plansitUser;
+	var newUsers []models.PlansitUser;
 	_, err  := q.GetAll(c, &newUsers)
 	if err != nil{
 		return 
 	}
 	if newUsers != nil{
-		currentUser= newUsers[0]
+		currentUser= &newUsers[0]
 		currentUser.Userid = "1111"
 	}
 }
@@ -121,5 +120,6 @@ func placeKey(c appengine.Context) *datastore.Key {
         // The string "default_guestbook" here could be varied to have multiple guestbooks.
         return datastore.NewKey(c, "Place", "default_place", 0, nil)
 }
+
 
 
