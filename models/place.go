c8a9f1ca76192d864f7a92231352928f842fb49c
Benaldo Hernandez <bhernandez@tonicdesign.com>
2015-03-25 22:48:27 -0400

    create protocol buffer for transferrring user data. Update plansit ot use these

--- models/place.go
+++ models/place.go
@@ -2,15 +2,20 @@ package models
 
 import(
 	"time"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"github.com/golang/protobuf/proto"
 )
-
-type Place struct{
-	Place_id string
-	Notes string
-	UserID string
+type PlansitUser struct {
+	Userid string
+	Name string
+	Email string
+	Trips []Trip
 }
 
 type Trip struct{
+	Id string
 	Name string
 	DateCreated time.Time
 	Description string
@@ -18,10 +23,91 @@ type Trip struct{
 	Length	string
 	Places []Place
 }
-type PlansitUser struct {
-	Userid string
-	Name string
-	Email string
-	Trips []Trip
+type Place struct{
+	Placeid string
+	Notes string
+	UserID string
+}
+type DBCommit struct{
+	UserID string
+	UserData []byte
+}
+
+var CurrentUser *PlansitUser
+var C appengine.Context
+
+func Init(c appengine.Context){
+	C=c
+	u:= user.Current(C)
+    GetUser(u.ID)
+    if CurrentUser == nil{
+    	CurrentUser = create(u.ID, u.Email, u.Email)
+    }   
+}
+func create(userid string, name string, email string) *PlansitUser{
+	CurrentUser = &PlansitUser{userid, name, email, nil}
+	save()
+	return CurrentUser
+}
+func GetUser(userid string){
+	load(userid)
+}
+func AddTrip(name string, desc string, departure time.Time, length string){
+
+}
+func RemoveTrip(id string){
+
+}
+func GetTrip(id string) Trip{
+	return CurrentUser.Trips[0]
+}
+
+func AddPlace(tripid string, placeId string, notes string){
+	//add place here
+}
+func GetPlaces(tripID string) []Place {
+	return GetTrip(tripID).Places
+}
+func RemovePlace(placeId string, tripName string){
+}
+func  load (userid string) { 
+	q := datastore.NewQuery("User").Ancestor(userKey(C)).Filter("UserID=", userid)
+	var newUsers []DBCommit;
+	_, err  := q.GetAll(C, &newUsers)
+	if err != nil{
+		return 
+	}
+	if newUsers != nil{
+		data := newUsers[0].UserData
+		protoUser := new(PlansItProto)
+		err = proto.Unmarshal(data, protoUser)
+		if err != nil {
+			C.Infof("unmarshaling error: ", err)
+			return
+		}
+		CurrentUser = &PlansitUser{protoUser.GetGoogleId(), protoUser.GetName(), protoUser.GetEmail(), nil}
+	}
+		
+}
+
+func save(){
+	protoUser := new(PlansItProto)
+	protoUser.GoogleId = proto.String(CurrentUser.Userid)
+	protoUser.Email = proto.String(CurrentUser.Email)
+	data, err := proto.Marshal(protoUser)
+	if err != nil {
+		C.Infof("marshaling error: ", err)
+	}
+	dbObject := &DBCommit{CurrentUser.Userid, data}
+	
+	key := datastore.NewIncompleteKey(C, "User", userKey(C))
+	_, error := datastore.Put(C, key, dbObject)
+    if error !=nil{
+    	C.Infof(error.Error())
+    	return
+    }
+}
+func userKey(c appengine.Context) *datastore.Key {
+        return datastore.NewKey(c, "User", "default_user", 0, nil)
 }
 
--- plansit.go
+++ plansit.go
@@ -6,9 +6,7 @@ import(
 	"html/template"
 	"models"
 	"appengine"
-	"appengine/user"
-	"appengine/datastore"
-	"time"
+_	"time"
 )
 
 var (
@@ -27,9 +25,10 @@ func init(){
 
 func root(w http.ResponseWriter, r *http.Request){
 	checkAuth(w,r)
-	myPlaces := GetPlace(r)
+	models.Init(appengine.NewContext(r))
+	myUser := models.CurrentUser
 
-	indexTemplate.Execute(w, myPlaces)
+	indexTemplate.Execute(w, myUser)
 }
 
 func mapper( w http.ResponseWriter, r *http.Request){
@@ -37,89 +36,19 @@ func mapper( w http.ResponseWriter, r *http.Request){
 	mapperTemplate.Execute(w, "")
 }
 func addPlace(w http.ResponseWriter, r * http.Request){
- 	placeid, notes := r.FormValue("placeid"), r.FormValue("notes")
- 	checkAuth(w, r)
- 	place := &models.Place{placeid, notes, currentUser.Userid}	
- 	PutPlace(r, place)
-
- 	indexTemplate.Execute(w, placeid +  " "  + notes + " " + currentUser.Email)
-
+ 	placeid, notes, tripid := r.FormValue("placeid"), r.FormValue("notes") ,
+ 	r.FormValue("tripid")
+ 	models.Init(appengine.NewContext(r))
+ 	models.AddPlace(tripid, placeid,notes)	
 }
 
 func checkAuth(w http.ResponseWriter, r *http.Request){ 
 	c:= appengine.NewContext(r)
-	u:= user.Current(c)
-	if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return 
-    }
-    GetUser(r, u)
-    if currentUser != nil{
-    	var myTrip []models.Trip
-    	var myPlaces []models.Place
-    	myPlaces[0] = models.Place{"91981981","a nice place","111111"}
-    	myTrip[0] = models.Trip{"My trip",time.Now(), "my tupe is gonna be awesome", time.Now(), "15 days", myPlaces }
-    	newUser := &models.PlansitUser{u.ID, "hi", u.Email, nil}
-    	PutUser(r, newUser)
-   	currentUser = &models.PlansitUser{u.ID, "hi", u.Email, nil}
-   }
+	models.Init(c)
 }
 
-func PutUser(r *http.Request, myUser *models.PlansitUser){
-	c:= appengine.NewContext(r)
-	key := datastore.NewIncompleteKey(c, "User", userKey(c))
-	_, err := datastore.Put(c, key, myUser)
-    if err !=nil{
-    	return
-    }
-}
 
-func GetUser(r *http.Request, u *user.User) {
-	c:= appengine.NewContext(r)
-	q := datastore.NewQuery("User").Ancestor(userKey(c)).Filter("Userid=", u.ID)
-	var newUsers []models.PlansitUser;
-	_, err  := q.GetAll(c, &newUsers)
-	if err != nil{
-		return 
-	}
-	if newUsers != nil{
-		currentUser= &newUsers[0]
-		currentUser.Userid = "1111"
-	}
-}
-func PutPlace(r *http.Request, place *models.Place){
-	c:= appengine.NewContext(r)
-  	key := datastore.NewIncompleteKey(c, "Place", placeKey(c))
-    _, err := datastore.Put(c, key, place)
-    if err !=nil{
-    	return
-    }
-}
-func GetPlace(r *http.Request) []models.Place{
-    c:= appengine.NewContext(r)
-    q := datastore.NewQuery("Place").Ancestor(placeKey(c)).Limit(10)
-    var places []models.Place
-    _, err := q.GetAll(c, &places)
-    if err !=nil{
-    	return nil
-    }
-    return places
-}
 
-func userKey(c appengine.Context) *datastore.Key {
-        // The string "default_guestbook" here could be varied to have multiple guestbooks.
-        return datastore.NewKey(c, "User", "default_user", 0, nil)
-}
-func placeKey(c appengine.Context) *datastore.Key {
-        // The string "default_guestbook" here could be varied to have multiple guestbooks.
-        return datastore.NewKey(c, "Place", "default_place", 0, nil)
-}
 
 
 
